1be10864d6948571dc46669f4da6fe4501dc5372
eva-barczykowska <eva.barczykowska@gmail.com>
2020-09-15 16:14:54 +0200

    finishing getting started chapter from the course

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,13 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+    <h1>Hello, React!</h1>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Some files and what they contain:
+// index.html: The HTML page template.
+// index.js: The entry point of our app.
+// style.css: the stylesheet for our project.
+// package.json: holds various metadata relevant to the project, like dependencies.
